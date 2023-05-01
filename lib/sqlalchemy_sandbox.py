635cad8ae27daabea974e3c9b77ae21fc01b0fff
Enoch2k2 <enoch2k2@hotmail.com>
2023-05-01 10:15:02 -0500

    finished

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -7,12 +7,17 @@ Base = declarative_base()
 
 
 class Student(Base):
+    # NOTE: declare what the table name is
     __tablename__ = 'students'
 
+    # NOTE: declare the columns
     id = Column(Integer(), primary_key=True)
     name = Column(String())
 
 
+# NOTE: Still not sure what this is?
 if __name__ == '__main__':
+    # NOTE: declare what the database is
     engine = create_engine('sqlite:///students.db')
+    # NOTE: create the databases
     Base.metadata.create_all(engine)
